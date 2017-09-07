360b3c9e2e0f871ea785034a17f41ea21d6e04ee
khinthuzaraung <nanhazelaunghein@gmail.com>
2017-09-07 10:50:21 +0630

    first commit

--- resources/views/login.html.php
+++ resources/views/login.html.php
@@ -79,7 +79,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 					</div>
 					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
 						<ul class="nav navbar-nav">
-							<li><a href="index.html">Home</a></li>	
+							<li><a href="welcome.blade.php">Home</a></li>	
 							<!-- Mega Menu -->
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Products <b class="caret"></b></a>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/',function(){
+Route::get('/login',function(){
 	return view('login');
 });
