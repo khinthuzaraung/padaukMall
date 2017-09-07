fd4ece8982216be51455d7fa4b48c60986dbf39b
Zwe Man Soe <ss.kaunghtike@gmail.com>
2017-09-07 10:43:32 +0630

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login',function(){
-	return view('login');
+Route::get('register',function(){
+	return view('register');
 });
